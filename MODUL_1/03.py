67726c96351cfca34b458e08f2a965b3691aefca
L200180020 <48055224+L200180020@users.noreply.github.com>
2020-03-11 18:35:08 +0700

    Add files via upload

---
+++ MODUL_1/03.py
@@ -0,0 +1,10 @@
+
+def jumlahHurufKonsonan(x):
+    konsonan = "BCDFGHJKLMNPQRSTVWXYZbcdfghjklmnpqrstvwxyz"
+    a = len(x)
+    b = ""
+    for k in x:
+        if k in konsonan:
+            b+=k
+    c = len(b)
+    return (a,c)
