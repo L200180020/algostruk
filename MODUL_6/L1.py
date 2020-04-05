ef31b38521de3ccd4a1f1dc0c672b2ca1909f5ff
L200180020 <mrnfaris@gmail.com>
2020-04-05 11:44:25 +0700

    Add files via upload

---
+++ MODUL_6/L1.py
@@ -0,0 +1,19 @@
+def gabungkanDuaListUrut(A,B):
+    la=len(A); lb=len(B)
+    C=list()
+    i=0; j=0
+
+    while i < la and j < lb:
+        if A[i] < B[j]:
+            C.append(A[i])
+            i += 1
+        else:
+            C.append(A[i])
+            j += 1
+    while i < la:
+        C.append(A[i])
+        j += 1
+    while j < lb:
+        C.append(B[j])
+        j += 1
+    return C
